fac19e348e47ded9ccd38c9068dc14f4e51aa9c3
Aitor Beriain <beriain@bitmessage.ch>
2016-10-18 17:10:05 +0200

    Remove unused import

--- app/src/main/java/eu/polarclock/MainActivity.java
+++ app/src/main/java/eu/polarclock/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
