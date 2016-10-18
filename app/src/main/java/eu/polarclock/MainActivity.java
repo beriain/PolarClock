5ed8f967b1963a2380f083874fd2f8211572cbc5
beriain <beriain@bitmessage.ch>
2016-10-18 17:07:17 +0200

    Fix back navigation in android

--- app/src/main/java/eu/polarclock/MainActivity.java
+++ app/src/main/java/eu/polarclock/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         WebView wv = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = wv.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -19,4 +23,13 @@ public class MainActivity extends AppCompatActivity {
         //webSettings.setUseWideViewPort(true);
         wv.loadUrl("file:///android_asset/polar-clock.html");
     }
+
+	@Override
+	public void onBackPressed()
+	{
+        WebView wv = (WebView) findViewById(R.id.webView);
+        if(wv.getUrl().compareToIgnoreCase("file:///android_asset/polar-clock.html") != 0)
+            wv.loadUrl("javascript:goBack()");
+        else finish();
+	}
 }
