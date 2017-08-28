2dbea0e324c75e67ff5b9df6dc35f5c0e3d89544
beriain <beriain@bitmessage.ch>
2017-08-28 20:35:52 +0200

    Remove appcompat to reduce apk size

--- app/src/main/java/eu/polarclock/MainActivity.java
+++ app/src/main/java/eu/polarclock/MainActivity.java
@@ -1,17 +1,17 @@
 package eu.polarclock;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         WebView wv = (WebView) findViewById(R.id.webView);
