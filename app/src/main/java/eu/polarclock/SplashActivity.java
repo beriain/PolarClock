1df511ae3200aef8539d444c3652f2e310ce6de7
Mikel <beriain@bitmessage.ch>
2018-02-03 13:47:06 +0100

    Update icon and add splash screen

---
+++ app/src/main/java/eu/polarclock/SplashActivity.java
@@ -0,0 +1,17 @@
+package eu.polarclock;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
